e0dfedb83a70fc667338b3f8e2e4a82d68fa39bb
manishdask <manishkd120@gmail.com>
2025-09-01 22:08:39 +1000

    Update dbserver.js

--- dbserver.js
+++ dbserver.js
@@ -12,18 +12,28 @@ const User = mongoose.model('User', new mongoose.Schema({
   course: String
 }));
 
-// GET API to fetch user
-app.get('/user', async (req, res) => {
+// post api to add new user
+app.post ( '/user' , async (req, res) =>{
+
+const user = new User (req.body);
+await user.save();
+res.send ("user saved!");
+})
+
+
+// GET user by name
+app.get('/users/:name', async (req, res) => {
   
   const user = await User.findOne({ name: "Manish" });
 
-  // If user exists, send it; otherwise send a message
+  // If user exists, send it, otherwise send a message
   if (user) {
     res.json(user);
   } else {
     res.json({ message: "User not found" });
   }
 });
+
 
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
